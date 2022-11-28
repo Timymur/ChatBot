7ef0c886218b1529874a7ac9c63e3819c78bac6e
Timymur <105367869+Timymur@users.noreply.github.com>
2022-11-28 18:52:58 +0500

    update

--- main.py
+++ main.py
@@ -7,15 +7,7 @@ from telebot import types
 
 
 
-mydb = mysql.connector.connect(
 
-    host = "localhost",
-    user = 'root',
-    password = ""
-)
-myCur = mydb.cursor()
-sql = "CREATE DATABASE IF NOT EXISTS NoteBook"
-myCur.execute(sql)
 
 
 db = mysql.connector.connect(
@@ -28,12 +20,8 @@ db = mysql.connector.connect(
 
 cur = db.cursor()
 
-cur.execute(''' CREATE TABLE IF NOT EXISTS users (
-    id int auto_increment primary key,
-    name varchar(30) UNIQUE ,  
-    num varchar(20),
-    adres TEXT
-) ''')
+
+
 
 
 # Функция для проверки имени на существование
@@ -69,6 +57,24 @@ def prop(message):
     bot.send_message(message.chat.id, 'Если захочешь выполнить какие действия с блокнотом, жми на кнопку, всегда буду рад помочь))) ', reply_markup=markup)
 
 
+@bot.message_handler(commands=['GiveTheCategory'])
+def category(message):
+    m = f'1 - Семья; \n 2 - Друзья;\n 3 - Знакомые; \n 4 - Коллеги; \n 5 - Одногруппники; \n 6 - Другое; \n Введи цифру' \
+
+
+    bot.send_message(message.chat.id, m)
+
+
+@bot.message_handler(commands=['no'])
+def otkaz(message):
+    bot.send_message(message.chat.id, 'Как хочешь')
+
+
+@bot.message_handler(commands=['AddEvent'])
+def event(message):
+    bot.send_message(message.chat.id, 'Напиши через запятую : "Событие,, `какое-то содержание`, `YYYY.MM.DD`" (Дату указывать в таком формате , как показано в шаблоне).\n(После слово СОБЫТИЕ стоят две запятые, там должно быть имя(необязательно), если его не указать, то событие привяжется к последнему добавленному пользователю)')
+
+
 
 @bot.message_handler(content_types=["text"])
 def noteBook(message):
@@ -80,6 +86,10 @@ def noteBook(message):
             cur.execute(sql, data)
             db.commit()
             bot.send_message(message.chat.id, 'Контакт успешно добавлен')
+            m = f'Хотите присвоить контакту  одну из категорий?(Семья, друзья, знакомые, коллеги, одногруппники, другое) \n'\
+                                              f'/GiveTheCategory\n'\
+                                              f'/no'
+            bot.send_message(message.chat.id, m)
         except Exception:
             txt = "Контакт с таким именем уже существует"
             bot.send_message(message.chat.id, txt)
@@ -125,6 +135,45 @@ def noteBook(message):
             bot.send_message(message.chat.id, txt)
 
 
+
+
+    elif len(data) == 4:
+        data[0] = data[0].lower()
+        data[0] = data[0].strip()
+        data[1] = data[1].strip()
+        data[2] = data[2].strip()
+        data[3] = data[3].strip()
+        if data[0]=='событие':
+            if data[1]=='':
+                sql = "SELECT id FROM users WHERE id  =  (SELECT MAX(id) FROM users)"
+                cur.execute(sql)
+                id = cur.fetchone()
+                id = id[0]
+                sql = "INSERT INTO notes( user_id, note, date) VALUES (%s, %s, %s)"
+                cor = [id, data[2], data[3]]
+                cur.execute(sql, cor)
+                db.commit()
+                bot.send_message(message.chat.id, "ОК. Все готово. ")
+            else :
+                cor = (data[1],)
+                checkNameVar = checkName(cor)
+                if checkNameVar == True:
+
+                    sql = 'SELECT id from users WHERE name = %s'
+                    cor = (data[1],)
+                    cur.execute(sql, cor)
+                    id = cur.fetchone()
+                    id = id[0]
+                    sql = "INSERT INTO notes( user_id, note, date) VALUES (%s, %s, %s)"
+                    cor = [id, data[2], data[3]]
+                    cur.execute(sql, cor)
+                    db.commit()
+                    bot.send_message(message.chat.id, "ОК. Все готово. ")
+
+
+                else:
+                    txt = "Контакта не существует"
+                    bot.send_message(message.chat.id, txt)
 
 
     elif len(data) == 5:
@@ -181,6 +230,76 @@ def noteBook(message):
             for user in users:
                 txt = (f'Имя: {user[1]}  \n  Номер: {user[2]} \n Адрес: {user[3]} ')
                 bot.send_message(message.chat.id, txt)
+
+        elif message.text.strip()  == '1':
+            sql = "SELECT id FROM users WHERE id  =  (SELECT MAX(id) FROM users)"
+            cur.execute(sql)
+            id = cur.fetchone()
+            id = id[0]
+            sql = "INSERT INTO typeOfRelationShip( id_user, TOR) VALUES (%s, %s)"
+            cor = [id, "Семья"]
+            cur.execute(sql, cor)
+            db.commit()
+            bot.send_message(message.chat.id, "ОК. Все готово. Хочешь добавить какое-нибудь событие к этому человеку?/AddEvent\n/no")
+
+        elif message.text.strip()  == '2':
+            sql = "SELECT id FROM users WHERE id  =  (SELECT MAX(id) FROM users)"
+            cur.execute(sql)
+            id = cur.fetchone()
+            id = id[0]
+            sql = "INSERT INTO typeOfRelationShip( id_user, TOR) VALUES (%s, %s)"
+            cor = [id, "Друзья"]
+            cur.execute(sql, cor)
+            db.commit()
+            bot.send_message(message.chat.id,
+                             "ОК. Все готово. Хочешь добавить какое-нибудь событие к этому человеку?/AddEvent\n/no")
+
+        elif message.text.strip()  == '3':
+            sql = "SELECT id FROM users WHERE id  =  (SELECT MAX(id) FROM users)"
+            cur.execute(sql)
+            id = cur.fetchone()
+            id = id[0]
+            sql = "INSERT INTO typeOfRelationShip( id_user, TOR) VALUES (%s, %s)"
+            cor = [id, "Знакомые"]
+            cur.execute(sql, cor)
+            db.commit()
+            bot.send_message(message.chat.id,
+                             "ОК. Все готово. Хочешь добавить какое-нибудь событие к этому человеку?/AddEvent\n/no")
+
+        elif message.text.strip()  == '4':
+            sql = "SELECT id FROM users WHERE id  =  (SELECT MAX(id) FROM users)"
+            cur.execute(sql)
+            id = cur.fetchone()
+            id = id[0]
+            sql = "INSERT INTO typeOfRelationShip( id_user, TOR) VALUES (%s, %s)"
+            cor = [id, "Коллеги"]
+            cur.execute(sql, cor)
+            db.commit()
+            bot.send_message(message.chat.id, "ОК. Все готово. Хочешь добавить какое-нибудь событие к этому человеку?/AddEvent\n/no")
+
+        elif message.text.strip()  == '5':
+            sql = "SELECT id FROM users WHERE id  =  (SELECT MAX(id) FROM users)"
+            cur.execute(sql)
+            id = cur.fetchone()
+            id = id[0]
+            sql = "INSERT INTO typeOfRelationShip( id_user, TOR) VALUES (%s, %s)"
+            cor = [id, "Одногруппники"]
+            cur.execute(sql, cor)
+            db.commit()
+            bot.send_message(message.chat.id, "ОК. Все готово. Хочешь добавить какое-нибудь событие к этому человеку?/AddEvent\n/no")
+
+        elif message.text.strip()  == '6':
+            sql = "SELECT id FROM users WHERE id  =  (SELECT MAX(id) FROM users)"
+            cur.execute(sql)
+            id = cur.fetchone()
+            id = id[0]
+            sql = "INSERT INTO typeOfRelationShip( id_user, TOR) VALUES (%s, %s)"
+            cor = [id, "Другое"]
+            cur.execute(sql, cor)
+            db.commit()
+            bot.send_message(message.chat.id,
+                             "ОК. Все готово. Хочешь добавить какое-нибудь событие к этому человеку?/AddEvent\n/no")
+
         else:
             txt = "Я не понимаю"
             bot.send_message(message.chat.id, txt)
